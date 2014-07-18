97c6aa383b79b040c9c7adf1401801c89330be6e
Jens Christian Hillerup <jens@hillerup.net>
2014-07-18 16:04:03 +0200

    added color blob tracking to the app

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,39 @@
 #include <queue>
 #include <stdio.h>
 #include <math.h>
+#include <vector>
 
 #include "constants.h"
 #include "findEyeCenter.h"
 #include "findEyeCorner.h"
 
+using namespace std;
+using namespace cv;
 
 /** Constants **/
 
 
 /** Function Headers */
 void detectAndDisplay( cv::Mat frame );
+void findFacialMarkers( cv::Mat frame);
 
 /** Global variables */
 //-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
-cv::String face_cascade_name = "../../../res/haarcascade_frontalface_alt.xml";
+cv::String face_cascade_name = "../../res/haarcascade_frontalface_alt.xml";
 cv::CascadeClassifier face_cascade;
 std::string main_window_name = "Capture - Face detection";
 std::string face_window_name = "Capture - Face";
+std::string markers_window_name = "Capture - Facial Markers";
 cv::RNG rng(12345);
 cv::Mat debugImage;
 cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
+
+int iLowH = 48;
+int iHighH = 65;
+int iLowS = 115; 
+int iHighS = 255;
+int iLowV = 178;
+int iHighV = 247;
 
 /**
  * @function main
@@ -34,7 +46,7 @@ cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
 int main( int argc, const char** argv ) {
   CvCapture* capture;
   cv::Mat frame;
-
+  
   // Load the cascades
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
 
@@ -42,51 +54,84 @@ int main( int argc, const char** argv ) {
   cv::moveWindow(main_window_name, 400, 100);
   cv::namedWindow(face_window_name,CV_WINDOW_NORMAL);
   cv::moveWindow(face_window_name, 10, 100);
+  cv::namedWindow(markers_window_name,CV_WINDOW_NORMAL);
+  cv::moveWindow(markers_window_name, 800, 100);
+  
   cv::namedWindow("Right Eye",CV_WINDOW_NORMAL);
   cv::moveWindow("Right Eye", 10, 600);
   cv::namedWindow("Left Eye",CV_WINDOW_NORMAL);
   cv::moveWindow("Left Eye", 10, 800);
-  cv::namedWindow("aa",CV_WINDOW_NORMAL);
-  cv::moveWindow("aa", 10, 800);
-  cv::namedWindow("aaa",CV_WINDOW_NORMAL);
-  cv::moveWindow("aaa", 10, 800);
 
+  // Add trackbars for the HSV settings
+  cvCreateTrackbar("LowH",  markers_window_name.c_str(), &iLowH, 179); //Hue (0 - 179)
+  cvCreateTrackbar("HighH", markers_window_name.c_str(), &iHighH, 179);
+  cvCreateTrackbar("LowS",  markers_window_name.c_str(), &iLowS, 255); //Saturation (0 - 255)
+  cvCreateTrackbar("HighS", markers_window_name.c_str(), &iHighS, 255);
+  cvCreateTrackbar("LowV",  markers_window_name.c_str(), &iLowV, 255); //Value (0 - 255)
+  cvCreateTrackbar("HighV", markers_window_name.c_str(), &iHighV, 255);
+ 
   createCornerKernels();
   ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2),
           43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
 
-   // Read the video stream
-  capture = cvCaptureFromCAM( -1 );
+  // Read the video stream
+  //capture = cvCaptureFromFile("../../res/jc.jpg" );
+  capture = cvCaptureFromCAM(2);
   if( capture ) {
+
     while( true ) {
       frame = cvQueryFrame( capture );
-      // mirror it
       cv::flip(frame, frame, 1);
+      
+      // mirror it
       frame.copyTo(debugImage);
 
       // Apply the classifier to the frame
       if( !frame.empty() ) {
-        detectAndDisplay( frame );
+        findFacialMarkers(frame);
+        
+        detectAndDisplay(frame);
       }
       else {
         printf(" --(!) No captured frame -- Break!");
         break;
       }
 
-      imshow(main_window_name,debugImage);
+      imshow(main_window_name, debugImage);
 
       int c = cv::waitKey(10);
       if( (char)c == 'c' ) { break; }
       if( (char)c == 'f' ) {
         imwrite("frame.png",frame);
       }
-
     }
   }
 
   releaseCornerKernels();
 
   return 0;
+}
+
+void findFacialMarkers(cv::Mat frame) {
+  vector<vector<cv::Point> > contours;
+  vector<cv::Vec4i> hierarchy;
+  
+  cv::Mat imgHSV;
+  cv::cvtColor(frame, imgHSV, cv::COLOR_RGB2HSV); //Convert the captured frame from RGB to HSV
+
+  cv::Mat imgThresholded;
+  cv::inRange(imgHSV, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+  imshow(markers_window_name.c_str(), imgThresholded);
+
+  cv::findContours(imgThresholded, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE, cv::Point(0, 0));
+
+  for (int i = 0; i < contours.size(); i++) {
+    Rect bound = boundingRect(contours[i]);
+    Point center = Point( bound.x + (bound.width / 2), bound.y + (bound.height / 2));
+
+    circle(debugImage, center, 3, Scalar(0, 0, 255), -1);
+  }
 }
 
 void findEyes(cv::Mat frame_gray, cv::Rect face) {
@@ -162,9 +207,9 @@ void findEyes(cv::Mat frame_gray, cv::Rect face) {
   }
 
   imshow(face_window_name, faceROI);
-//  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
-//  cv::Mat destinationROI = debugImage( roi );
-//  faceROI.copyTo( destinationROI );
+  //  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
+  //  cv::Mat destinationROI = debugImage( roi );
+  //  faceROI.copyTo( destinationROI );
 }
 
 
@@ -176,11 +221,11 @@ cv::Mat findSkin (cv::Mat &frame) {
 
   for (int y = 0; y < input.rows; ++y) {
     const cv::Vec3b *Mr = input.ptr<cv::Vec3b>(y);
-//    uchar *Or = output.ptr<uchar>(y);
+    //    uchar *Or = output.ptr<uchar>(y);
     cv::Vec3b *Or = frame.ptr<cv::Vec3b>(y);
     for (int x = 0; x < input.cols; ++x) {
       cv::Vec3b ycrcb = Mr[x];
-//      Or[x] = (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) > 0) ? 255 : 0;
+      //      Or[x] = (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) > 0) ? 255 : 0;
       if(skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) == 0) {
         Or[x] = cv::Vec3b(0,0,0);
       }
@@ -198,19 +243,19 @@ void detectAndDisplay( cv::Mat frame ) {
 
   std::vector<cv::Mat> rgbChannels(3);
   cv::split(frame, rgbChannels);
-  cv::Mat frame_gray = rgbChannels[2];
+  cv::Mat frame_gray = rgbChannels[2]; // Used to be rgbChannels[2], i.e. the blue channel.
 
   //cvtColor( frame, frame_gray, CV_BGR2GRAY );
   //equalizeHist( frame_gray, frame_gray );
   //cv::pow(frame_gray, CV_64F, frame_gray);
   //-- Detect faces
   face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
-//  findSkin(debugImage);
+  //  findSkin(debugImage);
 
   for( int i = 0; i < faces.size(); i++ )
-  {
-    rectangle(debugImage, faces[i], 1234);
-  }
+    {
+      rectangle(debugImage, faces[i], 1234);
+    }
   //-- Show what you got
   if (faces.size() > 0) {
     findEyes(frame_gray, faces[0]);
