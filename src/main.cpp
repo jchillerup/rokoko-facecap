e7b7b3531ed46aebe5822ed3cc3300a70a17141b
Jens Christian Hillerup <jens@hillerup.net>
2014-08-20 17:56:15 +0200

    Added a #define so the GUI can be disabled

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define SHOW_GUI
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -18,7 +19,6 @@ using namespace cv;
 #include "lo/lo.h"
 #include "rokoko-common.c"
 
-
 /** Function Headers */
 void detectAndDisplay( cv::Mat frame, rokoko_face* );
 void findFacialMarkers( cv::Mat frame, rokoko_face* );
@@ -29,10 +29,14 @@ void dispatch_osc(rokoko_face*);
 //-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
 cv::String face_cascade_name = "../../res/haarcascade_frontalface_alt.xml";
 cv::CascadeClassifier face_cascade;
+
+#ifdef SHOW_GUI
 std::string main_window_name = "Capture - Face detection";
 std::string face_window_name = "Capture - Face";
 std::string markers_window_name = "Capture - Facial Markers";
 std::string markers_window_name_alt = "Capture - Facial Markers, alternative";
+#endif
+
 cv::RNG rng(12345);
 cv::Mat debugImage;
 cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
@@ -45,7 +49,7 @@ int iLowV = 64;
 int iHighV = 255;
 int contourAreaMin = 100;
 bool rotateCam = false;
-lo_address recipient = lo_address_new("127.0.0.1", "14040");
+lo_address recipient = lo_address_new("192.168.0.115", "14040");
 
 /**
  * @function main
@@ -57,13 +61,14 @@ int main( int argc, const char** argv ) {
   // Load the cascades
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
 
+#ifdef SHOW_GUI
   cv::namedWindow(main_window_name,CV_WINDOW_NORMAL);
   cv::moveWindow(main_window_name, 400, 100);
   cv::namedWindow(face_window_name,CV_WINDOW_NORMAL);
   cv::moveWindow(face_window_name, 10, 100);
   cv::namedWindow(markers_window_name,CV_WINDOW_NORMAL);
   cv::moveWindow(markers_window_name, 800, 100);
-
+  
   // Add trackbars for the HSV settings
   cvCreateTrackbar("LowH",  markers_window_name.c_str(), &iLowH, 179); //Hue (0 - 179)
   cvCreateTrackbar("HighH", markers_window_name.c_str(), &iHighH, 179);
@@ -72,7 +77,8 @@ int main( int argc, const char** argv ) {
   cvCreateTrackbar("LowV",  markers_window_name.c_str(), &iLowV, 255); //Value (0 - 255)
   cvCreateTrackbar("HighV", markers_window_name.c_str(), &iHighV, 255);
   cvCreateTrackbar("ContourAreaMin", markers_window_name.c_str(), &contourAreaMin, 1000);
-
+#endif
+  
   createCornerKernels();
   ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2),
           43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
@@ -107,7 +113,7 @@ int main( int argc, const char** argv ) {
 
       // Make an OSC packet with the face data.
       dispatch_osc(&cur_face);
-
+#ifdef SHOW_GUI
       imshow(main_window_name, debugImage);
 
       int c = cv::waitKey(10);
@@ -115,6 +121,7 @@ int main( int argc, const char** argv ) {
       if( (char)c == 'f' ) {
         imwrite("frame.png",frame);
       }
+#endif
     }
   }
 
@@ -126,8 +133,6 @@ int main( int argc, const char** argv ) {
 
 
 void dispatch_osc(rokoko_face* cur_face) {
-  pretty_print_face(cur_face);
-
   lo_blob blob = lo_blob_new(sizeof(rokoko_face), cur_face);
   lo_send(recipient, "/face", "b", blob);
 }
@@ -144,8 +149,9 @@ void findFacialMarkers(cv::Mat frame, rokoko_face* cur_face) {
   cv::Mat imgThresholded;
   cv::inRange(imgHSV, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
+#ifdef SHOW_GUI
   imshow(markers_window_name.c_str(), imgThresholded);
-
+#endif
   // TODO: Apply erosion/dilation to imgThresholded?
 
   cv::findContours(imgThresholded, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE, cv::Point(0, 0));
@@ -178,7 +184,9 @@ void findFacialMarkersOld(cv::Mat frame) {
   cv::bitwise_and(rgbChannels[1] > mingreen, rgbChannels[1] > (rgbChannels[2] * gor), greens);
   cv::bitwise_and(greens, rgbChannels[1] > (rgbChannels[0] * gor), greens);
 
+#ifdef SHOW_GUI
   imshow(markers_window_name_alt.c_str(), greens);
+#endif
 }
 
 void findEyes(cv::Mat frame_gray, cv::Rect face, rokoko_face* cur_face) {
@@ -220,19 +228,26 @@ void findEyes(cv::Mat frame_gray, cv::Rect face, rokoko_face* cur_face) {
   rightRightCornerRegion.x += rightPupil.x;
   rightRightCornerRegion.height /= 2;
   rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
+
+  #ifdef SHOW_GUI
   rectangle(debugFace,leftRightCornerRegion,200);
   rectangle(debugFace,leftLeftCornerRegion,200);
   rectangle(debugFace,rightLeftCornerRegion,200);
   rectangle(debugFace,rightRightCornerRegion,200);
+  #endif
+  
   // change eye centers to face coordinates
   rightPupil.x += rightEyeRegion.x;
   rightPupil.y += rightEyeRegion.y;
   leftPupil.x += leftEyeRegion.x;
   leftPupil.y += leftEyeRegion.y;
+
+  #ifdef SHOW_GUI
   // draw eye centers
   circle(debugFace, rightPupil, 3, 1234);
   circle(debugFace, leftPupil, 3, 1234);
-
+  #endif
+  
   // Put the coordinates into our face structure
   cur_face->left_eye = leftPupil;
   cur_face->right_eye = rightPupil;
@@ -251,13 +266,17 @@ void findEyes(cv::Mat frame_gray, cv::Rect face, rokoko_face* cur_face) {
     cv::Point2f rightRightCorner = findEyeCorner(faceROI(rightRightCornerRegion), false, false);
     rightRightCorner.x += rightRightCornerRegion.x;
     rightRightCorner.y += rightRightCornerRegion.y;
+#ifdef SHOW_GUI
     circle(faceROI, leftRightCorner, 3, 200);
     circle(faceROI, leftLeftCorner, 3, 200);
     circle(faceROI, rightLeftCorner, 3, 200);
     circle(faceROI, rightRightCorner, 3, 200);
+#endif
   }
 
+#ifdef SHOW_GUI  
   imshow(face_window_name, faceROI);
+#endif
   //  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
   //  cv::Mat destinationROI = debugImage( roi );
   //  faceROI.copyTo( destinationROI );
@@ -294,7 +313,7 @@ void detectAndDisplay( cv::Mat frame, rokoko_face* cur_face) {
 
   std::vector<cv::Mat> rgbChannels(3);
   cv::split(frame, rgbChannels);
-  cv::Mat frame_gray = rgbChannels[2];
+  cv::Mat frame_gray = rgbChannels[1];
 
   //cvtColor( frame, frame_gray, CV_BGR2GRAY );
   //equalizeHist( frame_gray, frame_gray );
@@ -304,7 +323,9 @@ void detectAndDisplay( cv::Mat frame, rokoko_face* cur_face) {
   //  findSkin(debugImage);
 
   for( int i = 0; i < faces.size(); i++ ) {
+#ifdef SHOW_GUI
     rectangle(debugImage, faces[i], 1234);
+#endif
   }
   //-- Show what you got
   if (faces.size() > 0) {
